defd237cf48ae62067ba3e6b8b9d6f45fe9385b7
maneldomel <emanuelmilk25@gmail.com>
2025-08-05 19:54:47 -0300

    Increase main product image size only

--- src/App.tsx
+++ src/App.tsx
@@ -94,7 +94,7 @@ function App() {
                 <img 
                   src="https://i.imgur.com/VScX6oJ.png" 
                   alt="6 Bottles of Proaxion" 
-                  className="w-80 h-80 md:w-[28rem] md:h-[28rem] lg:w-[32rem] lg:h-[32rem] object-contain mx-auto"
+                  className="w-96 h-96 md:w-[36rem] md:h-[36rem] lg:w-[40rem] lg:h-[40rem] object-contain mx-auto"
                 />
               </div>
               
